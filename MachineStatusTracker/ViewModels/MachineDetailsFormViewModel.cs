38388e1c4cf2639097f30868b7cf36ab1af01553
Liat Nativ <liatnativ@gmail.com>
2024-06-17 22:28:58 +0300

    Added view models for the submit and edit operations

---
+++ MachineStatusTracker/ViewModels/MachineDetailsFormViewModel.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace MachineStatusTracker.ViewModels
+{
+    public class MachineDetailsFormViewModel:ViewModelBase
+    {
+        private string _machineName;
+        public string MachineName {
+            get
+            { 
+                return _machineName; 
+            } 
+            set 
+            {
+               _machineName = value;
+                OnPropertyChanged(nameof(MachineName));
+                OnPropertyChanged(nameof(CanSubmit));
+            } 
+        }
+
+        private string _machineStatus;
+        public string MachineStatus
+        {
+            
+        get
+            {
+                return _machineStatus;
+            }
+            set
+            {
+                _machineName = value;
+                OnPropertyChanged(nameof(MachineStatus));
+                OnPropertyChanged(nameof(CanSubmit));
+            }
+        }
+
+        private string _machineDescription;
+        public string MachineDescription
+        {
+
+            get
+            {
+                return _machineStatus;
+            }
+            set
+            {
+                _machineName = value;
+                OnPropertyChanged(nameof(MachineDescription));
+            }
+        }
+
+        public ICommand SubmitCommand { get; set; }
+        public ICommand CancelCommand { get; set; }
+
+        public bool CanSubmit => !string.IsNullOrEmpty(MachineStatus) && !string.IsNullOrEmpty(MachineName);
+    }
+}
