0c19be7652a2a34d73178ab368b2ee2b7a62d906
Liat Nativ <liatnativ@gmail.com>
2024-06-18 22:26:51 +0300

    Added load commands

---
+++ MachineStatusTracker/Commands/LoadMachineStatusesCommand.cs
@@ -0,0 +1,32 @@
+﻿using MachineStatusTracker.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineStatusTracker.Commands
+{
+    public class LoadMachineStatusesCommand : AsyncCommandBase
+    {
+        private readonly MachineStore _machineStore;
+
+        public LoadMachineStatusesCommand(MachineStore machineStore)
+        {
+            _machineStore = machineStore;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await _machineStore.LoadStauses();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
---
+++ MachineStatusTracker/Commands/LoadMachinesCommand.cs
@@ -0,0 +1,33 @@
+﻿using MachineStatusTracker.Stores;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineStatusTracker.Commands
+{
+    public class LoadMachinesCommand : AsyncCommandBase
+    {
+
+        private readonly MachineStore _machineStore;
+
+        public LoadMachinesCommand(MachineStore machineStore)
+        {
+            _machineStore = machineStore;
+        }
+
+        public override async Task ExecuteAsync(object? parameter)
+        {
+            try
+            {
+                await _machineStore.LoadMachines();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+            
+        }
+    }
+}
