7f63e721f0e53d70a3289e362a1033289d3c325c
Liat Nativ <liatnativ@gmail.com>
2024-06-18 22:20:08 +0300

    Updated Store

--- MachineStatusTracker/Stores/MachineStore.cs
+++ MachineStatusTracker/Stores/MachineStore.cs
@@ -19,6 +19,16 @@ namespace MachineStatusTracker.Stores
         private readonly GetAllMachineQuery _findAllMachineQuery;
         private readonly GetAllMachineStatusesQuery _getAllMachineStatusesQuery;
 
+        private readonly List<Machine> _machines;
+        private readonly List<Status> _statuses;
+        public IEnumerable<Machine> Machines => _machines;
+        public IEnumerable<Status> Statuses => _statuses;
+
+        public event Action MachinesLoaded;
+        public event Action StatusesLoaded;
+        public event Action<Machine> MachineAdded;
+        public event Action<Machine> MachineUpdated;
+
         public MachineStore(ICreateMachineCommand createMachineCommand, 
             IUpdateMachineCommand updateMachineCommand, 
             IDeleteMachineCommand deleteMachineCommand, 
@@ -30,20 +40,50 @@ namespace MachineStatusTracker.Stores
             _deleteMachineCommand = deleteMachineCommand;
             _findAllMachineQuery = findAllMachineQuery;
             _getAllMachineStatusesQuery = getAllMachineStatusesQuery;
+            _machines = new List<Machine>();
+            _statuses = new List<Status>();
         }
 
-        public event Action<Machine> MachineAdded;
-        public event Action<Machine> MachineUpdated;
+
         public async Task Add(Machine machine)
         {
             await _createMachineCommand.Execute(machine);
+            _machines.Add(machine);
             MachineAdded?.Invoke(machine);
         }
 
         public async Task Update(Machine machine)
         {
             await _updateMachineCommand.Execute(machine);
+            int currentIndex =_machines.FindIndex(y => y.Id == machine.Id);
+            if (currentIndex == -1)
+            {
+                _machines[currentIndex] = machine;
+            }
+            else
+            {
+                _machines.Add(machine);
+            }
+
             MachineUpdated?.Invoke(machine);
+        }
+
+        public async Task LoadMachines()
+        {
+            IEnumerable<Machine> machines = await _findAllMachineQuery.Execute();  
+            _machines.Clear();
+            _machines.AddRange(machines);
+
+            MachinesLoaded?.Invoke();
+        }
+
+        public async Task LoadStauses()
+        {
+            IEnumerable<Status> statuses = await _getAllMachineStatusesQuery.Execute();
+            _statuses.Clear();
+            _statuses.AddRange(statuses);
+
+            StatusesLoaded?.Invoke();
         }
     }
 }
