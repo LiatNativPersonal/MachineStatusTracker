776e2f971d9216b614e4d60cb0a552a71954cfc5
Liat Nativ <liatnativ@gmail.com>
2024-06-18 14:07:20 +0300

    Added commands and queries interface

---
+++ MachineStatusTracker/Models/Queries/IGetAllMachines.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MachineStatusTracker.Models.Queries
+{
+    public interface IGetAllMachines
+    {
+        Task<IEnumerable<Machine>> Execute();
+    }
+}
